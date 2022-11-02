13f8a10ff2bbff3d452fa5fcbbe7db5a2ffc7fae
KazuoOkiyama <101231817+KazuoOkiyama@users.noreply.github.com>
2022-11-02 12:24:05 +0900

    fixed bug

--- database.py
+++ database.py
@@ -10,7 +10,7 @@ import asyncio
 MONGO_API_KEY = config('MONGO_API_KEY')
 
 client = motor.motor_asyncio.AsyncIOMotorClient(MONGO_API_KEY)
-client.get_io_loop = asyncio.get_event_loop()
+client.get_io_loop = asyncio.get_event_loop
 database = client.API_DB
 collection_todo = database.todo
 collection_user = database.user
